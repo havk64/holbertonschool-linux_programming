b2e43491cddf02d4d342e8cc3b78e8c7da57f980
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-06 23:18:26 -0300

    Task 1, first commit Script to create a shared library

---
+++ 0x00-c_dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Creates a dynamic library from all the source files in the current directory
+# shellcheck disable=SC2086
+
+SRC=$(find . -name '*.c' -printf '%P\n')
+CFLAGS="-Wall -Wextra -Werror -pedantic -fPIC"
+
+while read -r source; do
+    gcc ${CFLAGS} -c "$source"
+done <<< "$SRC"
+
+OBJS=$(find . -name '*.o' -printf "%P ")
+
+gcc -shared -o liball.so ${OBJS}
