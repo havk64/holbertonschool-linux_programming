65f6787164de4228fd0674589b290bf79a06c014
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-15 19:10:01 -0300

    Add docstring to _strcpy

--- 0x00-c_dynamic_libraries/src/strcpy.c
+++ 0x00-c_dynamic_libraries/src/strcpy.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
 
+/**
+ * _strcpy - concatenate two strings into a new one
+ * @dest: the destination string
+ * @src: the source string
+ *
+ * Return: a new string resulting of the concatenation of dest + src
+ */
 char *_strcpy(char *dest, char *src)
 {
 	char *s;
