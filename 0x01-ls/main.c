6df799eec5afb6099a270016148e702b4902bda0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-24 19:31:06 -0300

    Add docstring to functions

--- 0x01-ls/main.c
+++ 0x01-ls/main.c
@@ -1,5 +1,12 @@
 #include "ls_header.h"
 
+/**
+ * main - entry point
+ * @ac: number of command line arguments
+ * @av: array of strings representing command line arguments
+ *
+ * Return: On success 0, on error 1 or 2
+ */
 int main(int ac, char *av[])
 {
 	if (ac < 2)
