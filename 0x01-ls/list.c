d5615ec280e09431b6de6ec868bc75bed6234974
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-26 00:14:26 -0300

    Remove unused functions

--- 0x01-ls/list.c
+++ 0x01-ls/list.c
@@ -81,24 +81,3 @@ void print_list(Dlist *list)
 	}
 	printf("\n");
 }
-
-/**
- * free_list - deallocate a linked list
- * @list: element of a linked list
- *
- * Return: Void
- */
-void free_list(Dlist *list)
-{
-	Dlist *node;
-	Dlist *tmp;
-
-	node = list;
-	while (node != NULL)
-	{
-		free(node->str);
-		tmp = node->next;
-		free(node);
-		node = tmp;
-	}
-}
--- 0x01-ls/mem.c
+++ 0x01-ls/mem.c
@@ -1,53 +1,22 @@
 #include "ls_header.h"
 
 /**
- * freemem - free an array of strings
- * @s: the array to be freed
- * @len: the length of the array
+ * free_list - deallocate a linked list
+ * @list: element of a linked list
  *
- * Return: void
+ * Return: Void
  */
-void freemem(char **s, int len)
+void free_list(Dlist *list)
 {
-	int i;
+	Dlist *node;
+	Dlist *tmp;
 
-	for (i = 0; i < len; i++)
-		free(s[i]);
-	free(s);
-}
-
-/**
- * salloc - allocate memory to an array of strings
- * @size: the array's size
- *
- * Return: On success: a pointer to the beggining of newly allocated array
- * On error: Log the error to stderr
- */
-char **salloc(int size)
-{
-	char **s;
-
-	s = malloc(size * sizeof(char *));
-	if (s == NULL)
-		perror("malloc error: ");
-	return (s);
-}
-
-/**
- * strcpalloc - allocate memory to a string
- * @src: the string to be allocated
- *
- * Return: string pointer to newly allocated space on memory
- */
-char *strcpalloc(char *src)
-{
-	char *dest;
-	size_t size;
-
-	size = (_strlen(src) + 1);
-	dest = malloc(size);
-	if (dest == NULL)
-		perror("malloc error: ");
-	_strcpy(dest, src);
-	return (dest);
+	node = list;
+	while (node != NULL)
+	{
+		free(node->str);
+		tmp = node->next;
+		free(node);
+		node = tmp;
+	}
 }
