6e220583cf02cb9453b90c1c86e996030f5d1614
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-06-25 22:41:17 -0300

    Remove unused functions

--- 0x01-ls/ls_header.h
+++ 0x01-ls/ls_header.h
@@ -10,7 +10,6 @@
 #include <pwd.h>
 #include <grp.h>
 #include <errno.h>
-int _putchar(char c);
 size_t _strlen(char *s);
 char *string_concat(char *s1, char *s2);
 char f_type(mode_t mode);
@@ -23,6 +22,5 @@ int parse_opt(int max, char **av);
 int parse_dir(char **src, int fcount, char *av, int len);
 int file_check(char *path);
 void print_dir(DIR *dir);
-int open_directory(char *name);
 int open_dir(char *name);
 #endif /* LS_HEADER_H */
--- 0x01-ls/utils.c
+++ 0x01-ls/utils.c
@@ -17,18 +17,6 @@ size_t _strlen(char *s)
 }
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
  * string_concat - concatenates two strings in a newly allocated
  * space in memory
  * @s1: The first string
