cbfbf710d384bbc4c280d23b45cba07541ab3f45
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-06 01:08:47 -0300

    Implement reallocation

--- 0x02-getline/_getline.c
+++ 0x02-getline/_getline.c
@@ -7,22 +7,31 @@
  *
  * Return: On success, 0. On error, 1
  */
-int read_fd(int fd, char *buf)
+int read_fd(int fd, char **buf)
 {
 	int i;
 	char c;
+	size_t size = BUF_SIZE;
 
+	*buf = malloc(sizeof(char) * size);
+	if (buf == NULL)
+		return (1);
 	c = 0;
 	for (i = 0; c != '\n'; i++)
 	{
 		if (read(fd, &c, READ_SIZE) == 0)
 		{
-			free(buf);
+			free(*buf);
 			return (1);
 		}
-		buf[i] = c;
+		if (i >= (int)size)
+		{
+			size += BUF_SIZE;
+			*buf = _realloc(*buf, size);
+		}
+		(*buf)[i] = c;
 	}
-	buf[i - 1] = 0;
+	(*buf)[i - 1] = 0;
 	return (0);
 }
 
