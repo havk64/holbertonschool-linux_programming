88a303a4ca0a24c952f6f660124305b3ac3bd158
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-29 14:12:23 -0300

    Add docstring to functions

--- 0x05-signals/0-handle_signal.c
+++ 0x05-signals/0-handle_signal.c
@@ -1,10 +1,21 @@
 #include "signals.h"
 
+/**
+ * handler - Prints a message when a signal is received
+ * @signum: the signal to be handled
+ *
+ * Return: always void
+ */
 void handler(int signum)
 {
 	printf("Gotcha! [%d]\n", signum);
 }
 
+/**
+ * handle_signal - set a handler for the signal SIGINT
+ *
+ * Return: On Success 0, On Error -1
+ */
 int handle_signal(void)
 {
 	if (signal(SIGINT, handler) == SIG_ERR)
