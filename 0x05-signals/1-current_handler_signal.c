8e918f41111ddbd7343dcd63b0dab92856019a9e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-31 23:32:31 -0300

    Add docstring to current_handler_signal function

--- 0x05-signals/1-current_handler_signal.c
+++ 0x05-signals/1-current_handler_signal.c
@@ -1,5 +1,11 @@
 #include "signals.h"
 
+/**
+ * current_handler_signal - Retrieves the current handler of the signal SIGINT
+ *
+ * Return: On succes, pointer to the current handler of SIGINT
+ * On failure, NULL
+ */
 void (*current_handler_signal(void))(int)
 {
 	void (*handler)(int);
