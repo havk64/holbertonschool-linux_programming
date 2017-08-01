c58cdc13a84a1344be2eef192eda21bc98b53f50
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-07-31 23:36:35 -0300

    Add docstring to functions

--- 0x05-signals/print_hello.c
+++ 0x05-signals/print_hello.c
@@ -1,10 +1,21 @@
 #include "signals.h"
 
+/**
+ * print_hello - Print a message when SIGINT is received
+ * @signum: the signal to be handled
+ *
+ * Return: always void
+ */
 void print_hello(int __attribute__((unused)) signum)
 {
 	printf("Hello :)\n");
 }
 
+/**
+ * set_print_hello - Set a handler to the SIGINT signal
+ *
+ * Return: always void
+ */
 void set_print_hello(void)
 {
 	signal(SIGINT, print_hello);
