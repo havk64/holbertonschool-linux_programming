878a2d869b9f9a47a9193ffb1819772f73ed9239
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-02 18:49:36 -0300

    Add unit tests to task 3

---
+++ 0x05-signals/3-main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "signals.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: EXIT_SUCCESS or EXIT_FAILURE
+ */
+int main(void)
+{
+	void (*handler)(int);
+	int i;
+
+	handler = current_handler_sigaction();
+	printf("Address of the current handler: %#lx\n", (unsigned long int)handler);
+
+	/* Set 'print_hello()` as the handler for SIGINT */
+	set_print_hello();
+
+	handler = current_handler_sigaction();
+	printf("Address of the 'print_hello' function: %#lx\n",
+	       (unsigned long int)&print_hello);
+	printf("Address of the current handler: %#lx\n", (unsigned long int)handler);
+
+	for (i = 0; ; i++)
+	{
+		printf("[%d] Wait for it ...\n", i);
+		sleep(1);
+	}
+	return (EXIT_SUCCESS);
+}
