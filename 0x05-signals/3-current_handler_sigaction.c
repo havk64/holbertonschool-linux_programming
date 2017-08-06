c1fde829064b6b0db17be9e2c212f03ed2dedd74
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-05 22:32:42 -0300

    Add docstring to current_handler_sigaction function

--- 0x05-signals/3-current_handler_sigaction.c
+++ 0x05-signals/3-current_handler_sigaction.c
@@ -1,5 +1,11 @@
 #include "signals.h"
 
+/**
+ * current_handler_sigaction - retrieves the current handler of signal SIGINT
+ *
+ * Return: On success, pointer to the current handler
+ * On error, NULL
+ */
 void (*current_handler_sigaction(void))(int)
 {
 	struct sigaction oldact;
