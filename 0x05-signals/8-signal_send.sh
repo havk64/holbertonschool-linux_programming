99c355f3dd5be6b9225d960462aa7f28b35a122f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-08 01:16:00 -0300

    Task 8, first commit

---
+++ 0x05-signals/8-signal_send.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Sends the signal SIGQUIT to a process, given its PID
+
+usage(){
+    printf "Usage: %s <pid>\n ${BASH_SOURCE}"
+    exit
+}
+
+[ "$#" != 1 ] && usage
+
+kill -SIGINT "$1"
+exit
