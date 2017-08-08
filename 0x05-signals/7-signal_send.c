974984492962948826dbeb8976bae357e7cc4ae2
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-08 00:32:57 -0300

    Task 7, first commit

---
+++ 0x05-signals/7-signal_send.c
@@ -0,0 +1,18 @@
+#include "signals.h"
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+
+	if (argc != 2)
+	{
+		printf("Usage: %s <pid>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+	pid = atoi(argv[1]);
+
+	if (kill(pid, SIGINT) < 0)
+		return (EXIT_FAILURE);
+
+	return (EXIT_SUCCESS);
+}
