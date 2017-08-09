b6fa903ee9c99dcc89aa8e563b7c8a818f8c7872
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-09 11:53:27 -0300

    Fix requirements, just one liner function

--- 0x05-signals/10-pid_exist.c
+++ 0x05-signals/10-pid_exist.c
@@ -9,10 +9,5 @@
  */
 int pid_exist(pid_t pid)
 {
-	if (kill(pid, 0) == 0)
-		return (1);
-	else if (errno == ESRCH)
-		return (0);
-	else
-		return (1);
+	return (kill(pid, 0) == 0 ? 1 : (errno == ESRCH) ? 0 : 1);
 }
