f7e02ce5ede6a9a1d3411e0a401b72139792d9fb
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-09 11:48:52 -0300

    Add docstring to pid_exist function

--- 0x05-signals/10-pid_exist.c
+++ 0x05-signals/10-pid_exist.c
@@ -1,6 +1,12 @@
 #include <signal.h>
 #include <errno.h>
 
+/**
+ * pid_exist - Check if a given pid exits
+ * @pid: the pid to check for
+ *
+ * Return: On success, 1. On error, 0
+ */
 int pid_exist(pid_t pid)
 {
 	if (kill(pid, 0) == 0)
