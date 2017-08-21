db454d99d200570f48b3e869eb925cd23be47973
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-08-20 21:45:08 -0300

    Include pid_exist prototype to signals.h

--- 0x05-signals/signals.h
+++ 0x05-signals/signals.h
@@ -17,4 +17,5 @@ void _sa_handler(int signum);
 void (*current_handler_sigaction(void))(int);
 int trace_signal_sender(void);
 void sigquit_handler(int signum, siginfo_t *info, void *);
+int pid_exist(pid_t pid);
 #endif /* SIGNALS_H */
