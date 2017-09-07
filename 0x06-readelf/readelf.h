2db1453ee56a4e97585d891d458d8734582d6b8a
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-06 22:45:48 -0300

    Add a struct to represent both 32 and 64 bits elf files

--- 0x06-readelf/readelf.h
+++ 0x06-readelf/readelf.h
@@ -8,6 +8,23 @@ typedef enum
 {
 	ELF32, ELF64
 } ElfClass;
+typedef struct ehdr
+{
+	unsigned char	e_ident[EI_NIDENT];	/* ELF "magic number" */
+	long		e_entry;		/* Entry point virtual address */
+	uint64_t	e_phoff;		/* Program header table file offset */
+	uint64_t	e_shoff;		/* Section header table file offset */
+	uint32_t	e_version;		/* Identifies object file version */
+	uint32_t	e_flags;		/* Processor-specific flags */
+	uint16_t	e_type;			/* Identifies object file type */
+	uint16_t	e_machine;		/* Specifies required architecture */
+	uint16_t	e_ehsize;		/* ELF header size in bytes */
+	uint16_t	e_phentsize;		/* Program header table entry size */
+	uint16_t	e_phnum;		/* Program header table entry count */
+	uint16_t	e_shentsize;		/* Section header table entry size */
+	uint16_t	e_shnum;		/* Section header table entry count */
+	uint16_t	e_shstrndx;		/* Section header string table index */
+} ElfN_Ehdr; /* Struct to represent headers from both 32 and 64bits files */
 #define width 40
 int is_elf(unsigned char *magic);
 int get_stat(char *filename);
