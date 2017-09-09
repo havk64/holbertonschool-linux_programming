ed90103399d8d0e877f9b6bf932b4c0fe89cb7a9
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-09 00:26:55 -0300

    Add docstring to functions on main file

--- 0x06-readelf/main_0.c
+++ 0x06-readelf/main_0.c
@@ -1,5 +1,12 @@
 #include "readelf.h"
 
+/**
+ * check - verify return value of system calls
+ * @n: return value
+ * @error: constant string to be printed in case of error
+ *
+ * Return, always Void
+ */
 static void check(int n, char *error)
 {
 	if (n < 0)
@@ -9,6 +16,11 @@ static void check(int n, char *error)
 	}
 }
 
+/**
+ * is_elf - verify the first bytes of an elf file
+ * @magic: the first bytes of an elf file
+ * Return: 1 to true and 0 to false
+ */
 int is_elf(unsigned char *magic)
 {
 	if (magic[0] == 0x7f &&
@@ -20,6 +32,11 @@ int is_elf(unsigned char *magic)
 		return (0);
 }
 
+/**
+ * get_stat - verify attributes of a file
+ * @filename: the name of the file to be checked
+ * Return: the file descriptor related to the open file
+ */
 int get_stat(char *filename)
 {
 	int fd, st;
@@ -39,11 +56,22 @@ int get_stat(char *filename)
 	return (fd);
 }
 
+/**
+ * get_class - check the class of a elf file
+ * @c: the byte to be checked
+ * Return: a ElfClass enum object
+ */
 ElfClass get_class(char c)
 {
 	return ((c == 2) ? ELF64 : ELF32);
 }
 
+/**
+ * main - entry point
+ * @argc: number of positional arguments
+ * @argv: the vector string
+ * Return: On success, EXIT_SUCCESS. On Error, EXIT_FAILURE
+ */
 int main(int argc, char *argv[])
 {
 	int fd;
