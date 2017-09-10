34fb744df5bc62588228e3825e5a90cc3c1854a4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-09 22:09:43 -0300

    Add docstring to functions

--- 0x06-readelf/print2.c
+++ 0x06-readelf/print2.c
@@ -1,5 +1,10 @@
 #include "readelf.h"
 
+/**
+ * print_elf_version - prints the elf version (default to the first one)
+ * @elfversion: the byte that represents the current elf version
+ * Return: Always void.
+ */
 void print_file_version(uint32_t elfversion)
 {
 	char *format = "  %-*s0x%x\n";
@@ -15,6 +20,11 @@ void print_file_version(uint32_t elfversion)
 	}
 }
 
+/**
+ * print_phoff - prints the program header offset
+ * @phoff: the program header table's file offset in bytes
+ * Return: Always void.
+ */
 void print_phoff(uint64_t phoff)
 {
 	char *title = "Start of program headers:";
@@ -23,6 +33,11 @@ void print_phoff(uint64_t phoff)
 	printf(format, width, title, phoff);
 }
 
+/**
+ * print_shoff - prints the section header offset
+ * @phoff: the section header table's file offset in bytes
+ * Return: Always void.
+ */
 void print_shoff(uint64_t shoff)
 {
 	char *title = "Start of section headers:";
