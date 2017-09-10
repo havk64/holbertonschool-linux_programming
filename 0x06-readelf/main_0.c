888ed097269dd1d493579950734f1fa8a6e8f878
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-10 15:37:10 -0300

    Transfer stat functions to separate file

--- 0x06-readelf/main_0.c
+++ 0x06-readelf/main_0.c
@@ -1,69 +1,6 @@
 #include "readelf.h"
 
 /**
- * check - verify return value of system calls
- * @n: return value
- * @error: constant string to be printed in case of error
- *
- * Return, always Void
- */
-static void check(int n, char *error)
-{
-	if (n < 0)
-	{
-		perror(error);
-		exit(EXIT_FAILURE);
-	}
-}
-
-/**
- * is_elf - verify the first bytes of an elf file
- * @magic: the first bytes of an elf file
- * Return: 1 to true and 0 to false
- */
-_Bool is_elf(unsigned char *magic)
-{
-	return (magic[0] == 0x7f &&
-		magic[1] == 0x45 &&
-		magic[2] == 0x4c &&
-		magic[3] == 0x46);
-}
-
-/**
- * get_stat - verify attributes of a file
- * @filename: the name of the file to be checked
- * Return: the file descriptor related to the open file
- */
-int get_stat(char *filename)
-{
-	int fd, st;
-	struct stat sbuf;
-
-	fd = open(filename, O_RDONLY);
-	check(fd, "Error opening file");
-
-	st = stat(filename, &sbuf);
-	check(st, "Stat error");
-
-	if ((sbuf.st_mode & S_IFMT) != S_IFREG)
-	{
-		printf("%s is not a regular file\n", filename);
-		exit(EXIT_FAILURE);
-	}
-	return (fd);
-}
-
-/**
- * get_class - check the class of a elf file
- * @c: the byte to be checked
- * Return: a ElfClass enum object
- */
-ElfClass get_class(char c)
-{
-	return ((c == 2) ? ELF64 : ELF32);
-}
-
-/**
  * main - entry point
  * @argc: number of positional arguments
  * @argv: the vector string
