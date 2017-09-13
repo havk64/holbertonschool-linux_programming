02400214575009ab759ee98e2057f5ff3f1f81b9
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-13 00:46:13 -0300

    Transfer get_strtab function to a separate file

--- 0x06-readelf/arch32.c
+++ 0x06-readelf/arch32.c
@@ -74,39 +74,3 @@ void parse_section_32(ElfN_Ehdr *ehdr, FILE *file, int ei_data)
 	free(sh_strtab);
 	free(shdr);
 }
-
-char *get_strtab(FILE *file, ElfN_Ehdr *ehdr)
-{
-	char *name;
-	ssize_t n;
-	Elf32_Shdr shdr;
-
-	n = fseek(file, ehdr->e_shoff + (ehdr->e_shstrndx * ehdr->e_shentsize),
-		  SEEK_SET);
-	if (n < 0)
-	{
-		perror("fseek error");
-		exit(EXIT_FAILURE);
-	}
-
-	n = fread(&shdr, sizeof(Elf32_Shdr), 1, file);
-	if (n != 1)
-		exit(EXIT_FAILURE);
-
-	name = malloc(shdr.sh_size);
-	if (name == NULL)
-		exit(EXIT_FAILURE);
-
-	n = fseek(file, shdr.sh_offset, SEEK_SET);
-	if (n < 0)
-	{
-		perror("fseek error");
-		exit(EXIT_FAILURE);
-	}
-
-	n = fread(name, 1, shdr.sh_size, file);
-	if (n != shdr.sh_size)
-		exit(EXIT_FAILURE);
-
-	return (name);
-}
