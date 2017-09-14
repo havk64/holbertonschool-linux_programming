56600741810230b34462524bb71c002e6ba8d2ec
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-14 05:07:28 -0300

    Add entry point to task 1, first commit

---
+++ 0x06-readelf/main_1.c
@@ -0,0 +1,32 @@
+#include "readelf.h"
+
+/**
+ * main - entry point
+ * @argc: number of positional arguments
+ * @argv: the vector string
+ * Return: On success, EXIT_SUCCESS. On Error, EXIT_FAILURE
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+	ElfN_Ehdr ehdr;
+	ElfN_Shdr *shdr;
+	FILE *stream;
+	char *strtab;
+
+	shdr = NULL;
+	if (argc != 2)
+	{
+		printf("Usage: %s <ELF binary file>\n", argv[0]);
+		return (EXIT_FAILURE);
+	}
+
+	fd = get_stat(argv[1]);
+	stream = parse_elf_sections(&ehdr, fd);
+	strtab = get_strtab(stream, &ehdr);
+	shdr = parse_test(stream, &ehdr);
+	print_sheader(shdr, &ehdr, strtab);
+
+	close(fd);
+	return (0);
+}
