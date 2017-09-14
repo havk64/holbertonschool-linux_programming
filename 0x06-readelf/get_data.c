0c0d3b3357e673b4cf865946f0ea9b680d0e8cb4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-13 20:51:06 -0700

    Add auxiliary functions to section header printing

--- 0x06-readelf/get_data.c
+++ 0x06-readelf/get_data.c
@@ -35,3 +35,39 @@ char *get_strtab(FILE *file, ElfN_Ehdr *ehdr)
 
 	return (name);
 }
+
+char get_letter(uint64_t flag)
+{
+	switch (flag)
+	{
+	case SHF_WRITE:			return ('W');
+	case SHF_ALLOC:			return ('A');
+	case SHF_EXECINSTR:		return ('X');
+	case SHF_MERGE:			return ('M');
+	case SHF_STRINGS:		return ('S');
+	case SHF_INFO_LINK:		return ('I');
+	case SHF_LINK_ORDER:		return ('L');
+	case SHF_OS_NONCONFORMING:	return ('O');
+	case SHF_GROUP:			return ('G');
+	case SHF_TLS:			return ('T');
+	case SHF_EXCLUDE:		return ('E');
+	default:
+		return (0);
+	}
+}
+
+char *get_flags(uint64_t sh_flags)
+{
+	int i;
+	static char buff[16];
+	uint64_t flag;
+
+	for (i = 0; sh_flags; i++)
+	{
+		flag = sh_flags & -sh_flags;
+		sh_flags &= ~flag;
+		buff[i] = get_letter(flag);
+	}
+	buff[i] = '\0';
+	return (buff);
+}
