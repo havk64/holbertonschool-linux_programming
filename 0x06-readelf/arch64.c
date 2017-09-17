5fd7a4da3cd1dbf252993cac296dc67e48730c56
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-17 20:31:23 -0300

    Refactoring parse functions

--- 0x06-readelf/arch64.c
+++ 0x06-readelf/arch64.c
@@ -26,53 +26,14 @@ void parse_64(ElfN_Ehdr *ehdr, FILE *file, int ei_data)
 	ehdr->e_shstrndx	= GET_BYTE(header.e_shstrndx);
 }
 
-void parse_section_64(ElfN_Ehdr *ehdr, ElfN_Shdr *shdr, FILE *file)
 {
 	ssize_t n;
-	uint64_t shnum;
-	Elf64_Shdr *xhdr;
 
-	shnum = ehdr->e_shnum;
-	xhdr = malloc(ehdr->e_shentsize * shnum);
-	if (xhdr == NULL)
 		exit(EXIT_FAILURE);
 
-	n = fseek(file, ehdr->e_shoff, SEEK_SET);
-	if (n < 0)
-	{
-		perror("Fseek error");
-		exit(EXIT_FAILURE);
-	}
-
-	n = fread(xhdr, sizeof(Elf64_Shdr), ehdr->e_shnum, file);
 	if (n != ehdr->e_shnum)
 		exit(EXIT_FAILURE);
 
-	copy_sheader64(shdr, xhdr, shnum, ehdr->e_ident[EI_DATA]);
-
-	free(xhdr);
-	fclose(file);
-}
-
-void
-copy_sheader64(ElfN_Shdr *shdr, Elf64_Shdr *xhdr, uint64_t shnum, int ei_data)
-{
-	uint64_t i;
-	uint64_t (*get_byte)(uint64_t, int);
-
-	get_byte = (ei_data == ELFDATA2MSB) ? get_byte_big_endian : get_byte_host;
-	for (i = 0; i < shnum; i++)
 	{
-		(shdr + i)->sh_name		= GET_BYTE((xhdr + i)->sh_name);
-		(shdr + i)->sh_type		= GET_BYTE((xhdr + i)->sh_type);
-		(shdr + i)->sh_flags		= GET_BYTE((xhdr + i)->sh_flags);
-		(shdr + i)->sh_addr		= GET_BYTE((xhdr + i)->sh_addr);
-		(shdr + i)->sh_offset		= GET_BYTE((xhdr + i)->sh_offset);
-		(shdr + i)->sh_size		= GET_BYTE((xhdr + i)->sh_size);
-		(shdr + i)->sh_link		= GET_BYTE((xhdr + i)->sh_link);
-		(shdr + i)->sh_info		= GET_BYTE((xhdr + i)->sh_info);
-		(shdr + i)->sh_addralign	= GET_BYTE((xhdr + i)->sh_addralign);
-		(shdr + i)->sh_entsize		= GET_BYTE((xhdr + i)->sh_entsize);
 	}
-
 }
