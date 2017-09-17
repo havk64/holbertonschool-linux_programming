8bf0a534fe818b28f3e273e523fa4dc900db1907
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-17 23:47:26 +0000

    Add unit test for task 1

--- 0x06-readelf/test_elf_header.sh
+++ 0x06-readelf/test_elf_header.sh
@@ -3,14 +3,23 @@
 FILE32=file32
 FILE64=0-hreadelf
 OBJFILE=main_0.o
-LD=/lib64/ld-linux-x86-64.so.2
+LDFILE=/lib64/ld-linux-x86-64.so.2
 
 make clean
 make 0-hreadelf
 
-diff -s <(readelf -h ${FILE32}) <(./0-hreadelf ${FILE32})
-diff -s <(readelf -h ${FILE64}) <(./0-hreadelf ${FILE64})
-diff -s <(readelf -h ${OBJFILE}) <(./0-hreadelf ${OBJFILE})
-diff -s <(readelf -h ${LD}) <(./0-hreadelf ${LD})
+diff -s <(readelf -W -h ${FILE32}) <(./0-hreadelf ${FILE32})
+diff -s <(readelf -W -h ${FILE64}) <(./0-hreadelf ${FILE64})
+diff -s <(readelf -W -h ${OBJFILE}) <(./0-hreadelf ${OBJFILE})
+diff -s <(readelf -W -h ${LDFILE}) <(./0-hreadelf ${LDFILE})
+
+make 1-hreadelf
+
+echo "Printing ${FILE32}"
+diff -s <(readelf -W -S ${FILE32}) <(./1-hreadelf ${FILE32})
+echo "Printing ${FILE64}"
+diff -s <(readelf -W -S ${FILE64}) <(./1-hreadelf ${FILE64})
+echo "Printing ${LDFILE}"
+diff -s <(readelf -W -S ${LDFILE}) <(./1-hreadelf ${LDFILE})
 
 exit
