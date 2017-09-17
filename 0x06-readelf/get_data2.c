5da8c708ca43431045b198aae2b825ee3c4d265e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-17 12:42:30 -0300

    Transfer alternative function get_flag to other file

---
+++ 0x06-readelf/get_data2.c
@@ -0,0 +1,34 @@
+#include "readelf.h"
+
+char *get_flags2(uint64_t sh_flags)
+{
+	static char buff[16];
+	int i;
+
+	i = 0;
+	if (sh_flags & SHF_WRITE)
+		buff[i++] = 'W';
+	if (sh_flags & SHF_ALLOC)
+		buff[i++] = 'A';
+	if (sh_flags & SHF_EXECINSTR)
+		buff[i++] = 'X';
+	if (sh_flags & SHF_MERGE)
+		buff[i++] = 'M';
+	if (sh_flags & SHF_STRINGS)
+		buff[i++] = 'S';
+	if (sh_flags & SHF_INFO_LINK)
+		buff[i++] = 'I';
+	if (sh_flags & SHF_LINK_ORDER)
+		buff[i++] = 'L';
+	if (sh_flags & SHF_OS_NONCONFORMING)
+		buff[i++] = 'O';
+	if (sh_flags & SHF_GROUP)
+		buff[i++] = 'G';
+	if (sh_flags & SHF_TLS)
+		buff[i++] = 'T';
+	if (sh_flags & SHF_EXCLUDE)
+		buff[i++] = 'E';
+
+	buff[i] = '\0';
+	return (buff);
+}
