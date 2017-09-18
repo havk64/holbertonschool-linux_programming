3b7343bd208505c8b3b94dd4bd569e53075997ad
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-18 14:33:05 -0300

    Backup alternative way to parse bit set flags

--- 0x06-readelf/get_data2.c
+++ 0x06-readelf/get_data2.c
@@ -37,3 +37,50 @@ char *get_flags2(uint64_t sh_flags)
 	buff[i] = '\0';
 	return (buff);
 }
+
+
+/**
+ * get_letter - returns a character representing each section header flag
+ * @flag: a bit flag
+ * Return: a character representing miscellaneous attributes
+ */
+char get_letter(uint64_t flag)
+{
+	switch (flag)
+	{
+	case SHF_WRITE:			return ('W');
+	case SHF_ALLOC:			return ('A');
+	case SHF_EXECINSTR:		return ('X');
+	case SHF_MERGE:			return ('M');
+	case SHF_STRINGS:		return ('S');
+	case SHF_INFO_LINK:		return ('I');
+	case SHF_LINK_ORDER:		return ('L');
+	case SHF_OS_NONCONFORMING:	return ('O');
+	case SHF_GROUP:			return ('G');
+	case SHF_TLS:			return ('T');
+	case SHF_EXCLUDE:		return ('E');
+	default:
+		return (0);
+	}
+}
+
+/**
+ * get_flags - retrieves the enabled attributes on a section header
+ * @sh_flags: a sh_flags member of a elf section header struct
+ * Return: The characters that identify the enabled attributes of an elf file
+ */
+char *get_flags(uint64_t sh_flags)
+{
+	int i;
+	static char buff[16];
+	uint64_t flag;
+
+	for (i = 0; sh_flags; i++)
+	{
+		flag = sh_flags & -sh_flags;
+		sh_flags &= ~flag;
+		buff[i] = get_letter(flag);
+	}
+	buff[i] = '\0';
+	return (buff);
+}
