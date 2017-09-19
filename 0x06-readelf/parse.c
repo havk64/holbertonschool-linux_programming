2f74d8b7f4fd5eb6a442b296538a111a36a95365
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-18 22:23:43 -0300

    Remove redundant function

--- 0x06-readelf/parse.c
+++ 0x06-readelf/parse.c
@@ -7,40 +7,6 @@
  * @fd: the file descriptor of the binary file
  * Return: Always Void
  */
-void parse_elf_header(ElfN_Ehdr *ehdr, int fd)
-{
-	FILE *stream;
-	void (*fill_Ehdr)(ElfN_Ehdr*, FILE*, unsigned char);
-	ssize_t n;
-	ElfClass elfclass;
-
-	stream = fdopen(fd, "rb");
-	if (stream == NULL)
-	{
-		perror("Error creating file stream");
-		exit(EXIT_FAILURE);
-	}
-
-	n = fread(ehdr->e_ident, EI_NIDENT, 1, stream);
-	if (!is_elf(ehdr->e_ident) || n != 1)
-	{
-		printf("Not an ELF file - it has the wrong magic bytes at the start\n");
-		exit(EXIT_FAILURE);
-	}
-
-	elfclass = get_class(ehdr->e_ident[EI_CLASS]);
-	fill_Ehdr = (elfclass == ELF32) ? &parse_32 : &parse_64;
-	(*fill_Ehdr)(ehdr, stream, ehdr->e_ident[EI_DATA]);
-	fclose(stream);
-}
-
-/**
- * parse_elf_sections - parses a elf file to print its reader
- * @ehdr: a pointer to an Elf header struct
- * @fd: the file descriptor of the binary file
- * Return: Always Void
- */
-FILE *parse_elf_sections(ElfN_Ehdr *ehdr, int fd)
 {
 	FILE *stream;
 	void (*fill_Ehdr)(ElfN_Ehdr*, FILE*, unsigned char);
--- 0x06-readelf/readelf.h
+++ 0x06-readelf/readelf.h
@@ -88,7 +88,6 @@ typedef struct ElfN_Shdr
 _Bool is_elf(unsigned char *magic);
 int get_stat(char *filename);
 void parse_elf_header(ElfN_Ehdr *ehdr, int fd);
-FILE *parse_elf_sections(ElfN_Ehdr *ehdr, int fd);
 ElfClass get_class(char c);
 void parse_32(ElfN_Ehdr *ehdr, FILE *file, unsigned char ei_data);
 void parse_64(ElfN_Ehdr *ehdr, FILE *file, unsigned char ei_data);
