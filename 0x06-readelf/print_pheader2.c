f4393089e8923891550487f8de84b96d1e817371
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-20 01:47:34 -0300

    Section to Segment mapping, first commit

--- 0x06-readelf/print_pheader2.c
+++ 0x06-readelf/print_pheader2.c
@@ -4,15 +4,25 @@ void
 print_segment_mapping(ElfN_Phdr *phdr, ElfN_Shdr *shdr, ElfN_Ehdr *ehdr,
 		      char *strtab)
 {
-	uint16_t i;
+	uint16_t i, j;
+	ElfN_Phdr segment;
+	ElfN_Shdr section;
 
-	(void)(phdr);
-	(void)(strtab);
 	printf("\n Section to Segment mapping:\n");
 	printf("  Segment Sections...\n");
 	for (i = 0; i < ehdr->e_phnum; i++)
 	{
 		printf("   %2.2u     ", i);
+		for (j = 0; j < ehdr->e_shnum; j++)
+		{
+			segment = *(phdr + i);
+			section = *(shdr + j);
+			if (section.sh_name != 0 &&
+			    section.sh_offset >= segment.p_offset &&
+			    section.sh_offset + section.sh_size <=
+			    segment.p_offset + segment.p_memsz)
+				printf("%s ", strtab + section.sh_name);
+		}
 		printf("\n");
 	}
 }
