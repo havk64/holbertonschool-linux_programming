fc9dde0c2125338eb996917b1ca3622853eda491
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-26 22:53:11 -0300

    Add EYNTK, step 4. Malloc Page

---
+++ 0x08-malloc/EYNTK/4-naive_malloc_page.c
@@ -0,0 +1,38 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void *naive_malloc_page(size_t size)
+{
+	static void *start_brk;
+	static char *ptr;
+	void *old_brk;
+	size_t *header, actual_size, chunk, hsize;
+	int pagesize;
+
+	(void)(actual_size);
+	if (start_brk == 0)
+	{
+		start_brk = sbrk(0);
+		ptr = (char *)start_brk;
+	}
+	hsize = sizeof(size_t);
+	chunk = size + hsize;
+	pagesize = sysconf(_SC_PAGESIZE);
+	if (pagesize < 0)
+	{
+		perror("sysconf error");
+		return (NULL);
+	}
+
+	if ((ptr + chunk) > (char *)sbrk(0))
+	{
+		old_brk = sbrk(pagesize);
+		printf("Old brk: %p\n", old_brk);
+	}
+	header = (size_t *)ptr;
+	*header = chunk;
+	ptr += chunk;
+	return ((void *)(ptr - size));
+}
