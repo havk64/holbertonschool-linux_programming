44c43e5efa301ac33ef29c2f28962afd6b3bd425
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-26 22:44:41 -0300

    Add EYNTK, step 2. Mmap example

---
+++ 0x08-malloc/EYNTK/1-main.c
@@ -0,0 +1,47 @@
+/* Private anonymous mapping example using mmap syscall */
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+static inline void errExit(void)
+{
+	perror("mmap failed");
+	exit(EXIT_FAILURE);
+}
+
+int main(void)
+{
+	int ret = -1;
+	char *addr;
+
+	printf("Private anonymous mapping example\n");
+	printf("Before mmap\n");
+	printf("(Open new terminal window and type:)\n");
+	printf("$ cat /proc/%d/maps\n", getpid());
+	getchar();
+
+	addr = NULL;
+	addr = mmap(NULL, (size_t)132 * 1024, PROT_READ | PROT_WRITE,
+		   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+	if (addr == MAP_FAILED)
+		errExit("mmap");
+
+	printf("After mmap\n");
+	printf("(Again...)\n");
+	getchar();
+
+	/* Unmap mapped region. */
+	ret = munmap(addr, (size_t)132 * 1024);
+	if (ret == -1)
+		errExit("munmap");
+	printf("After munmap\n");
+	printf("(Once more...)\n");
+	getchar();
+	printf("Bye!\n");
+	return (0);
+}
