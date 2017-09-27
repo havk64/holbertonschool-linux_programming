1fe64a85a78dda908bcebdc4adcb5d8a4679cc03
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-09-27 19:03:43 -0300

    Add EYNTK step 6, memory alignment

---
+++ 0x08-malloc/EYNTK/6-naive_malloc_align.c
@@ -0,0 +1,58 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define BRK_FAILED ((void *)-1)
+#define PAGESIZE (1 << 12)
+#define MSIZE 8
+#define ALIGN(size) ((size + (MSIZE - 1)) & -MSIZE)
+
+void *bootstrap(size_t *unused, size_t *header)
+{
+	void *brk;
+
+	brk = sbrk(PAGESIZE);
+	if (brk == BRK_FAILED)
+		return (NULL);
+
+	*unused = PAGESIZE;
+	header = (size_t *)brk;
+	*header = *unused;
+	return (brk);
+}
+
+void *naive_malloc_align(size_t size)
+{
+	static void *start_brk;
+	static size_t counter;
+	char *ptr, *old_brk;
+	size_t i, *header, chunk, hsize, unused;
+
+	counter += 1;
+	start_brk = (start_brk == 0) ? bootstrap(&unused, header = NULL) :
+		start_brk;
+	hsize = sizeof(size_t);
+	chunk = ALIGN(size) + hsize;
+
+	ptr = (char *)start_brk;
+	for (i = 0; i < counter; i++)
+	{
+		header = (size_t *)ptr;
+		if (i == (counter - 1))
+		{
+			unused = *header;
+			*header = chunk;
+		}
+		ptr += *header;
+	}
+
+	if (unused > chunk)
+	{
+		old_brk = sbrk(PAGESIZE);
+		unused += PAGESIZE;
+		(void)(old_brk);
+	}
+	header = (size_t *)ptr;
+	*header = unused - chunk;
+	return ((void *)(ptr - ALIGN(size)));
+}
