37d8961b46df1632aee991249ba0fd821abadffe
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-10 00:01:27 -0300

    Add comments

--- 0x09-libasm/EYNTK/add_me.asm
+++ 0x09-libasm/EYNTK/add_me.asm
@@ -1,10 +1,10 @@
 BITS 64
 
-	global add_me
+	global add_me 		; Define and export the add_me symbol
 
-	section .text
+	section .text		; Starts the .text (code) section/segment
 
-add_me:
-	mov eax, edi
-	add eax, esi
-	ret
+add_me:				; Implement the code to add_me symbol
+	mov eax, edi		; Copy the first argument to eax
+	add eax, esi		; Add the second argument to the previous value
+	ret			; Return
