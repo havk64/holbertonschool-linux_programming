3cd42f69329f67495d231521df2ab324294daadf
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-10 15:32:32 -0300

    Add solution to task 2, first commit

---
+++ 0x09-libasm/EYNTK/print_alphabet.asm
@@ -0,0 +1,44 @@
+BITS 64
+
+	global print_alphabet
+
+	section .text
+
+print_alphabet:
+	push rbp		; Funcion prologue
+	mov rbp, rsp
+
+	push rsi		; Save registers to the stack
+	push rax
+	push rdi
+	push rdx
+
+	sub rsp, 1		; Make room to our char variable (one byte)
+
+	;; Prepare the system call
+	mov rax, 1		; Number of write system call
+	mov rdi, 1		; Print to the STDOUT
+	mov rsi, rsp		; The address to read from
+	mov rdx, 1		; Size to be writed, one byte at a time
+
+	mov byte [rsp], 61h
+
+loop:				; Loop instructions
+	syscall
+	inc byte [rsp]
+	cmp byte [rsp], 7ah
+	jle loop
+	;; End of the loop
+
+	add rsp, 1		; Free the space used by our variable
+
+	pop rdx			; Restore register's values
+	pop rdi
+	pop rax
+	pop rsi
+
+	mov rsp, rbp		; Function epilogue
+	pop rbp
+	ret
+
+
