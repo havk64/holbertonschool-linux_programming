b49b6f23f29fc23c258f4ddaa57d2c0738d868ab
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-13 18:34:16 -0300

    Add docstring to _strcmp function

--- 0x00-c_dynamic_libraries/src/strcmp.c
+++ 0x00-c_dynamic_libraries/src/strcmp.c
@@ -1,5 +1,13 @@
 #include "holberton.h"
 
+/**
+ * _strcmp = compare two strings
+ * @s1: The first string to be compared
+ * @s2: The second string to be compared
+ * Return: an integer less than, equal to, or greater than zero if s1 (or the
+ * first n bytes thereof) is found, respectively, to be less than, to match, or
+ * be greater than s2.
+ */
 int _strcmp(char *s1,  char *s2)
 {
 	int n, i = 0;
