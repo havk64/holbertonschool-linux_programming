381b46c089fc8f0f5e7a4ef80c10529d2d8617fb
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-15 19:06:08 -0200

    Improve control flow

--- 0x09-libasm/1-strcmp.asm
+++ 0x09-libasm/1-strcmp.asm
@@ -16,20 +16,21 @@ loop:
 start:
 	movzx eax, BYTE [rdi + rcx]
 	test al, al
-	jz subtract
+	jz is_null
 	movzx edx, BYTE [rdi + rcx]
 	movzx eax, BYTE [rsi + rcx]
 	cmp dl, al
 	jz loop
-
-subtract:
-	movzx eax, BYTE [rdi + rcx]
-	movzx edx, al
-	movzx eax, BYTE [rsi + rcx]
-	movzx eax, al
-	cmp edx, eax
-	jl less_than
+return:	jl less_than
 	jg greater_than
+	jmp end
+
+less_than:
+	mov eax, -1
+	jmp end
+
+greater_than:
+	mov eax, 1
 
 end:
 	pop rcx
@@ -38,10 +39,9 @@ end:
 	pop rbp
 	ret
 
-less_than:
-	mov eax, -1
-	jmp end
+is_null:
+	movzx edx, BYTE [rdi + rcx]
+	movzx eax, BYTE [rsi + rcx]
+	sub al, dl
+	jmp return
 
-greater_than:
-	mov eax, 1
-	jmp end
