6a7cf8983ddfe524dbf8a74738e0bc32e12d407f
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-16 14:11:37 -0200

    Remove redundant tests/instructions, improved control flow

--- 0x09-libasm/1-strcmp.asm
+++ 0x09-libasm/1-strcmp.asm
@@ -19,22 +19,24 @@ asm_strcmp:
 	mov rbp, rsp
 	push rdx		; Save registers to be used
 	push rcx
-
 	mov rcx, 0		; Start a counter
-	jmp start		; Skip the first counter increment
 
 loop:
-	inc rcx			; Increment the counter
-start:	movzx eax, BYTE [rdi + rcx] ; Test if s1 char is NULL
-	test al, al		    ; 'test' (bitwise &) is lighter than 'cmp'
-	jz is_null		    ; If NULL (zero) exit from loop
+
 	movzx edx, BYTE [rdi + rcx] ; Char from first argument  (s1)
 	movzx eax, BYTE [rsi + rcx] ; Char from second argument (s2)
-	cmp dl, al		    ; If chars are equal keep looping
-	jz loop
-return:	jl less_than		; If first is less than second skip
+	cmp dl, al		    ; Compare values
+	jl less_than		; If first is less than second skip
 	jg greater_than		; If greater skip
-	jmp end			; Otherwise return
+	inc rcx			; Otherwise increment the counter
+	;; Test if the s1 character is NULL (end of string)
+	test al, al		; 'test' (bitwise &) is lighter than 'cmp'
+	jz is_equal		; If end of string (zero) exit from loop
+	jmp loop		; Otherwise keep looping
+
+is_equal:
+	mov eax, 0
+	jmp end
 
 less_than:
 	mov eax, -1		; If first less than second return -1
@@ -50,10 +52,4 @@ end:
 	pop rbp
 	ret
 
-is_null:
-	movzx eax, BYTE [rdi + rcx] ; If the first char is NULL check difference
-	movzx edx, BYTE [rsi + rcx] ; and act accordingly (jump to 'return')
-	sub al, dl
-	cmp al, 0
-	jmp return
 
