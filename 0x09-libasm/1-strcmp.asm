d235024f8a9f1426306ba8a03388e0d9d71dc7ba
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-16 00:30:59 -0200

    Update comments/explanations

--- 0x09-libasm/1-strcmp.asm
+++ 0x09-libasm/1-strcmp.asm
@@ -14,10 +14,10 @@ asm_strcmp:
 loop:
 	inc rcx			; Increment the counter
 start:	movzx eax, BYTE [rdi + rcx] ; Test if s1 char is NULL
-	test al, al		    ; 'test' is lighter than 'cmp'
-	jz is_null		    ; If NULL exit from loop
-	movzx edx, BYTE [rdi + rcx] ; Char from first argument
-	movzx eax, BYTE [rsi + rcx] ; Char from second argument
+	test al, al		    ; 'test' (bitwise &) is lighter than 'cmp'
+	jz is_null		    ; If NULL (zero) exit from loop
+	movzx edx, BYTE [rdi + rcx] ; Char from first argument  (s1)
+	movzx eax, BYTE [rsi + rcx] ; Char from second argument (s2)
 	cmp dl, al		    ; If chars are equal keep looping
 	jz loop
 return:	jl less_than		; If first is less than second skip
