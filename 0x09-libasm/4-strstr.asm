59c38345d403e546c413afe78825fcd8c7740ced
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-18 20:12:36 -0200

    Add solution for task 4, first commit

---
+++ 0x09-libasm/4-strstr.asm
@@ -0,0 +1,40 @@
+BITS 64
+CPU X64
+
+	global asm_strstr
+
+	segment .text
+
+asm_strstr:
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push rcx
+
+loop:
+	movzx eax, BYTE [edi]
+	cmp al, BYTE [esi]
+	jz match
+	inc edi
+	test al, al
+	jnz loop
+
+end:
+	pop rcx
+	pop rbx
+	mov rsp, rbp
+	pop rbp
+	ret
+
+match:
+	mov eax, edi
+	mov ecx, 0
+substr:
+	inc ecx
+	mov bl, BYTE [edi + ecx]
+	test bl, bl
+	jz end
+	cmp bl, BYTE [esi + ecx]
+	jz substr
+	inc edi
+	jmp loop
