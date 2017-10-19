9383ec8153f68bf3609a716e5c3a310a08d26c48
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-19 19:19:23 -0200

    Add solution for tasks 5, first commit

---
+++ 0x09-libasm/5-memcpy.asm
@@ -0,0 +1,31 @@
+BITS 64
+CPU X64
+	;; Prototype: void *asm_memcpy(void *dest, const void *src, size_t n);
+	global asm_memcpy
+
+	segment .text
+
+asm_memcpy:
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push rcx
+	mov rcx, 0
+	mov eax, edi
+
+loop:
+	cmp rdx, rcx
+	jz end
+	mov bl, BYTE [esi + ecx]
+	test bl, bl
+	jz end
+	mov [edi + ecx], bl
+	inc ecx
+	jmp loop
+
+end:
+	pop rcx
+	pop rbx
+	mov rsp, rbp
+	pop rbp
+	ret
