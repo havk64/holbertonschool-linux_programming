ad8b37f384fbcde1be22d8ae8c54348b22ca15a8
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-20 00:42:51 -0200

    RAX register is zeroed with bitwise xor

--- 0x09-libasm/0-strlen.asm
+++ 0x09-libasm/0-strlen.asm
@@ -6,12 +6,12 @@ BITS 64
 asm_strlen:
 	push rbp		; Function prologue
 	mov rbp, rsp
-	mov rax, 0		; Initialize the register counter
-	jmp start
+	xor eax, eax		; Initialize the counter
+	jmp start		; Skip the first increment
 
 loop:
-	inc rax
-start:	cmp BYTE [rdi + rax], 0
+	inc eax
+start:	cmp BYTE [edi + eax], 0
 	jnz loop		; End of the loop
 
 	mov rsp, rbp		; Function epilogue
