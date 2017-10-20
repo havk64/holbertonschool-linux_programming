f0c02556cf3b276d8973f4bfc3b68ccedf497a47
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-20 00:55:29 -0200

    Fix comments

--- 0x09-libasm/0-strlen.asm
+++ 0x09-libasm/0-strlen.asm
@@ -13,7 +13,7 @@ BITS 64
 	segment .text
 
 asm_strlen:
-	push rbp		; Function prologue
+	push rbp		; Routine prologue
 	mov rbp, rsp
 	xor eax, eax		; Initialize the counter
 	jmp start		; Skip the first increment
@@ -23,7 +23,7 @@ loop:
 start:	cmp BYTE [edi + eax], 0	; Compare each byte/char against zero
 	jnz loop		; If not zero (NULL/End of string) keep counting
 
-	mov rsp, rbp		; Function epilogue
+	mov rsp, rbp		; Routine epilogue
 	pop rbp
 	ret
 
