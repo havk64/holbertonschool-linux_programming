e653a948d1394e9e2fc25ca4e1dd1d46e0ff36f8
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-20 22:24:24 -0200

    Add solution for task 9, first commit

---
+++ 0x09-libasm/103-strncasecmp.asm
@@ -0,0 +1,55 @@
+	BITS 64
+	CPU X64
+
+	;; Prototype: int asm_strncasecmp(const char *s1, const char *s2, size_t n);
+	global asm_strncasecmp
+
+segment .text
+asm_strncasecmp:
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push rcx
+	xor ecx, ecx
+
+loop:
+	cmp edx, ecx
+	jz break
+	movzx eax, BYTE [edi + ecx]
+	cmp al, 0x5b
+	jb downcase1
+first:	movzx ebx, BYTE [esi + ecx]
+	cmp bl, 0x5b
+	jb downcase2
+cmp:	cmp al, bl
+	jnz diff
+	inc ecx
+	test al, al
+	jnz loop
+
+end:
+	pop rcx
+	pop rbx
+	mov rsp, rbp
+	pop rbp
+	ret
+
+diff:
+	sub eax, ebx
+	jmp end
+
+downcase1:
+	cmp al, 0x40
+	jna first
+	add al, 0x20
+	jmp first
+
+downcase2:
+	cmp bl, 0x40
+	jna cmp
+	add bl, 0x20
+	jmp cmp
+
+break:
+	mov eax, 0
+	jmp end
