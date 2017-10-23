b219fd822b879a81cee60915c53252eb6dbc16d9
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-22 19:53:45 -0700

    Add comments/explanation on source code

--- 0x09-libasm/3-strchr.asm
+++ 0x09-libasm/3-strchr.asm
@@ -6,24 +6,24 @@ CPU X64
 	segment .text
 
 asm_strchr:
-	push rbp
+	push rbp		; Routine prologue
 	mov rbp, rsp
-	push rcx
-	mov eax, edi
+	push rcx		; Save register to be used
+	mov eax, edi		; Copy the first function argument to eax register
 loop:
-	mov cl, BYTE [eax]
-	cmp ecx, esi
-	jz end
-	test cl, cl
-	jz eos
-	inc eax
-	jmp loop
+	mov cl, BYTE [eax]	; Read one byte of the string to low byte of ecx
+	cmp ecx, esi		; Compare it agains the second argument
+	jz end			; If they are equal jump to the end (returning eax)
+	test cl, cl		; Test if the byte/char is NULL (end of string)
+	jz eos			; If NULL jump to 'eos'
+	inc eax			; Otherwise increment the pointer to the string
+	jmp loop		; ...and loop again
 
 end:
-	pop rcx
+	pop rcx			; Routine epilogue
 	mov rsp, rbp
 	pop rbp
 	ret
 
-eos:	mov eax, 0
+eos:	mov eax, 0		; If end of string return 0
 	jmp end
