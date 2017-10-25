49cd622be6eac2edf46c5841abba4755b7f3db3d
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-24 21:54:49 -0700

    Add coments/explanations

--- 0x09-libasm/1000-puti.asm
+++ 0x09-libasm/1000-puti.asm
@@ -9,49 +9,55 @@ CPU X64
 	;; @n: the base 10 number to be printed
 	;;
 
-	global asm_puti
-	extern asm_putc
+	global asm_puti		; Define and export asm_puti symbol
+	extern asm_putc		; Import the asm_putc function/routine
 
 segment .text
-asm_puti:
-	push rbp
+asm_puti:			; Implement the asm_puti function/routine
+	push rbp		; Routine prologue, create new stack frame
 	mov rbp, rsp
-	push r9
-	xor r9d, r9d
-	mov eax, edi
-	test eax, eax
-	js negative
-num2str:
-	mov ebx, 10
-	xor ecx, ecx
-pushd:	xor edx, edx
-	div ebx
-	sub rsp, 1
-	mov [rsp], dl
-	inc cx
-	test eax, eax
-	jnz pushd
-print:	movzx edi, BYTE [rsp]
-	or dil, 0x30
-	call asm_putc
-	add r9b, al
-	add rsp, 1
-	dec cx
-	test cx, cx
-	jnz print
-	mov eax, r9d
+	push rbx		; Save registers to be used
+	push rcx
+	push rdx
+	push r8
+	xor r8d, r8d		; Reset 32 bits version of r9 register, our accumulator
+	mov eax, edi		; Copy 1st arg (number) to eax
+	test eax, eax		; Check the number sign
+	js negative		; If negative jump to subroutine
+num2str:			; Subroutine to convert number to string
+	mov ebx, 10		; Copy the divisor to ebx
+	xor ecx, ecx		; Start a counter
+pushd:	xor edx, edx		; Zero edx register used in the division
+	div ebx			; Divide the number (in eax) by 10
+	sub rsp, 1		; Reserve on byte on stack
+	mov [rsp], dl		; Save the remainder of the division to this byte
+	inc cx			; Increment the counter
+	test eax, eax		; Check when it reach zero
+	jnz pushd		; While not zero, loop
+print:	movzx edi, BYTE [rsp]	; Start retrieving each byte from stack, reverse order
+	or dil, 0x30		; Add 48 to each digit to convert it to an ASCII character
+	call asm_putc		; Call the routine to print each digit
+	add r8b, al		; Add the return value of function call to the accumulator
+	add rsp, 1		; Remove each byte from stack
+	dec cx			; Decrease the counter
+	test cx, cx		; Check when it reach zero
+	jnz print		; While not zero, loop
+	mov eax, r8d		; Transfer the accumulator to eax (return value)
 end:
-	pop r9
-	mov rsp, rbp
+	pop r8			; Restore registers
+	pop rdx
+	pop rcx
+	pop rbx
+	mov rsp, rbp		; Routine epilogue, take down the stack frame
 	pop rbp
 	ret
 
-negative:
-	neg eax
-	push rax
-	mov edi, '-'
-	call asm_putc
-	add r9b, al
-	pop rax
-	mov edi, eax
-	jmp num2str
+negative:			; Subroutine to deal with negative numbers
+	neg eax			; Flip sign
+	push rax		; Save the number to the stack
+	mov edi, '-'		; Prepare 1st function argument
+	call asm_putc		; Call putc to print the minus sign
+	add r8b, al		; Add the return value to the accumulator
+	pop rax			; Retrieve the number from stack
+	mov edi, eax		; Copy it to edi (1st argument to func call)
+	jmp num2str		; Jump to num2str subroutine
