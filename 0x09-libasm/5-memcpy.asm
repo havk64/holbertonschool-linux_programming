2832ed3608bb21d12ed9f75ff0a1b066889c0de2
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-26 04:42:59 -0700

    Add docstring to asm_memcpy routine

--- 0x09-libasm/5-memcpy.asm
+++ 0x09-libasm/5-memcpy.asm
@@ -1,6 +1,18 @@
 BITS 64
 CPU X64
+	;;
+	;; asm_memcpy - a clone of memcpy in x86 Assembly
+	;;
+	;; Description:	The memcpy() function copies @n bytes from memory area
+	;; @src to memory area @dest.
+	;;
+	;; @dest: a pointer to the destination string
+	;; @src: a pointer to the source string
+	;; @n: the number of bytes to be copied
+	;; Return: a pointer to @dest string
+	;;
 	;; Prototype: void *asm_memcpy(void *dest, const void *src, size_t n);
+	;;
 	global asm_memcpy
 
 	segment .text
