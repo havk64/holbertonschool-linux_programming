e902b911a9508af4aeef2f2e9bec117608a5632b
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-28 21:04:24 -0700

    Add docstring to asm_strcspn routine

--- 0x09-libasm/105-strcspn.asm
+++ 0x09-libasm/105-strcspn.asm
@@ -1,7 +1,19 @@
 BITS 64
 CPU X64
 
+	;;
+	;; asm_strcspn - clone of strcsnp in x86 Assembly
+	;;
+	;; Description:	calculates the length of the initial segment of @s which
+	;; consists entirely of bytes not in @reject.
+	;;
 	;; Prototype: size_t asm_strcspn(const char *s, const char *reject);
+	;; @s: a pointer to a string
+	;; @reject: a pointer to a substring
+	;; Return: the number of bytes in the initial segment of @s which are
+	;; not in the string @reject.
+	;;
+
 	global asm_strcspn
 
 segment .text
