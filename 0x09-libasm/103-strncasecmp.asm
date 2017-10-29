da512ca94602a448e33f9905f330cfe5110de7d8
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-28 20:53:06 -0700

    Add docstring to asm_strncasecmp routine

--- 0x09-libasm/103-strncasecmp.asm
+++ 0x09-libasm/103-strncasecmp.asm
@@ -1,7 +1,21 @@
 BITS 64
 CPU X64
 
+	;;
+	;; asm_strncasecmp - clone of strncasecmp function in X86 Assembly
+	;;
+	;; Description:	compares the two strings @s1 and @s2, ignoring the case
+	;; of the characters.
+	;;
 	;; Prototype: int asm_strncasecmp(const char *s1, const char *s2, size_t n);
+	;; @s1: a pointer to the first string
+	;; @s2: a pointer to the second string
+	;; @n: the number of bytes to be compared
+	;; Return: It returns an integer less than, equal to, or greater than
+	;; zero if s1 is found, respectively, to be less than, to match, or be
+	;; greater than s2.compare two strings ignoring case
+	;;
+
 	global asm_strncasecmp
 
 segment .text
