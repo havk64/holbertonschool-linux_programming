dec27c543af4de4317cd662e5a6efbbb2a4a14da
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-10-28 18:14:35 -0700

    Improve docstring ot asm_strlen

--- 0x09-libasm/0-strlen.asm
+++ 0x09-libasm/0-strlen.asm
@@ -1,18 +1,18 @@
 BITS 64
 
 	;;
-	;; strlen clone - find the length of a string
+	;; asm_strlen - clone of strlen function. Finds the length of a string
 	;;
 	;; Prototype: size_t asm_strlen(const char *str);
 	;; @str: a pointer to the string to be scanned
 	;; Return: the number of bytes of the given string
-	;; Using registers to count (no variables on stack frame)
+	;;
+	;; Uses registers to count (instead of variables on stack frame)
 	;;
 
 	global asm_strlen	; Export the symbol to be used by external files
 
 segment .text			; Define the beginning of the .text (code) segment
-
 asm_strlen:			; Define the asm_strlen routine
 	push rbp		; Routine prologue
 	mov rbp, rsp
