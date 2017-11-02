7e5f15959e016561ff64bbb29494aad1c982e505
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-11-01 23:24:44 -0700

    Improve strlen solution, using SSE 4.2 instruction (128 bits)

--- 0x09-libasm/0-strlen.asm
+++ 0x09-libasm/0-strlen.asm
@@ -12,19 +12,19 @@ BITS 64
 
 	global asm_strlen	; Export the symbol to be used by external files
 
-segment .text			; Define the beginning of the .text (code) segment
+section .text			; Define the beginning of the .text (code) section
 asm_strlen:			; Define the asm_strlen routine
 	push rbp		; Routine prologue
 	mov rbp, rsp
-	xor eax, eax		; Initialize the counter
-	jmp start		; Skip the first increment
+	pxor xmm0, xmm0		; reset xmm0 (NULL comparison)
+	mov eax, -16		; Initialize %eax to -16 to avoid one jmp
 
-loop:				; Define the symbol to be used by the jnz instruction
-	inc eax
-start:	cmp BYTE [edi + eax], 0	; Compare each byte/char against zero
-	jnz loop		; If not zero (NULL/End of string) keep counting
+loop:
+	add eax, 16		; Increment the counter by 16 each iteration
+	pcmpistri xmm0, [edi + eax], 1000b ; Look for the end of string
+	jnz short loop			   ; and generates an index on %ecx
 
+	add eax, ecx		; Add the index to the counter to be returned
 	mov rsp, rbp		; Routine epilogue
 	pop rbp
 	ret
-
