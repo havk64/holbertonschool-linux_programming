759f678a96a32675c3f209fb5da2fab389640f05
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-11-06 14:16:28 -0800

    Revert to version without SSE registers/instructions

--- 0x09-libasm/0-strlen.asm
+++ 0x09-libasm/0-strlen.asm
@@ -7,26 +7,23 @@ BITS 64
 	;; @str: a pointer to the string to be scanned
 	;; Return: the number of bytes of the given string
 	;;
-	;; Implementation using SSE 4.2 text processing instructions where
-	;; the string can be analyzed 16 bytes at a time thanks to the register size
-	;; of 128 bits, granting great performance.
+	;; Uses registers to count (instead of variables on stack frame)
 	;;
 
 	global asm_strlen	; Export the symbol to be used by external files
 
-section .text			; Define the beginning of the .text (code) section
+segment .text			; Define the beginning of the .text (code) segment
 asm_strlen:			; Define the asm_strlen routine
 	push rbp		; Routine prologue
 	mov rbp, rsp
-	pxor xmm0, xmm0		; reset xmm0 (NULL comparison)
-	mov eax, -16		; Initialize %eax to -16 to avoid one jmp
+	xor eax, eax		; Initialize the counter
+	jmp start		; Skip the first increment
 
-loop:
-	add eax, 16		; Increment the counter by 16 each iteration
-	pcmpistri xmm0, [edi + eax], 1000b ; Look for the end of string
-	jnz short loop			   ; and generates an index on %ecx
+loop:				; Define the symbol to be used by the jnz instruction
+	inc eax
+start:	cmp BYTE [edi + eax], 0	; Compare each byte/char against zero
+	jnz loop		; If not zero (NULL/End of string) keep counting
 
-	add eax, ecx		; Add the index to the counter to be returned
 	mov rsp, rbp		; Routine epilogue
 	pop rbp
 	ret
