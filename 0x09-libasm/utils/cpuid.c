6655aaf62eb479ab78ce366f9b2db2cc55bd11a0
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-11-08 21:22:06 -0800

    Get and print just the 32 lower bits of each register

--- 0x09-libasm/utils/cpuid.c
+++ 0x09-libasm/utils/cpuid.c
@@ -5,28 +5,28 @@
 
 int main(void)
 {
-	uint64_t rax, rbx, rcx, rdx;
+	uint32_t eax, ebx, ecx, edx;
 
 	/* Inline assembly to check CPU info */
 	/* *cpuid* is a processor suplementary instruction for x86 architecture  */
 	__asm__ __volatile__ ("cpuid\n\t"
-			      : "=a" (rax), "=b" (rbx), "=c" (rcx), "=d"(rdx)
+			      : "=a" (eax), "=b" (ebx), "=c" (ecx), "=d"(edx)
 			      : "0" (0));
 
 	/* Print hexadecimal representation of registers */
-	printf("rax: %08lx\n", rax);
-	printf("rbx: %08lx\n", rbx);
-	printf("rcx: %08lx\n", rcx);
-	printf("rdx: %08lx\n", rdx);
+	printf("eax: %08x\n", eax);
+	printf("ebx: %08x\n", ebx);
+	printf("ecx: %08x\n", ecx);
+	printf("edx: %08x\n", edx);
 
 	/* Print processor info */
 	printf("\nProcessor type: ");
-	printf("%s", (char *)&rbx);
-	printf("%s", (char *)&rdx);
-	printf("%s\n", (char *)&rcx);
+	printf("%.4s", (char *)&ebx);
+	printf("%.4s", (char *)&edx);
+	printf("%.4s\n", (char *)&ecx);
 	printf("Edx register as binary: ");
 	fflush(stdout);
-	asm_puti_base(rdx, "01");
+	asm_puti_base(edx, "01");
 	printf("\n");
 	return (0);
 }
