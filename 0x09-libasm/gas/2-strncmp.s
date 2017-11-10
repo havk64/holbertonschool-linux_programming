75e9dd591a5b92c59ca661609ded91da7ec9dab4
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-11-09 17:33:04 -0800

    Add 2-strcmp version with gas syntax

---
+++ 0x09-libasm/gas/2-strncmp.s
@@ -0,0 +1,54 @@
+
+	/*
+	 * strncmp clone - return the difference between two strings
+	 *
+	 * Prototype: int asm_strncmp(const char *s1, const char *s2, size_t n);
+	 * @s1: Pointer to the first string
+	 * @s2: Pointer to the second string
+	 * @n: the only first bytes to be compared
+	 * Return: an integer that less than, equal to, or greater than zero if @s1  is
+	 * found, respectively, to be less than, to match, or  be greater than s2.
+	 *
+	 * Implementation using just registers instead of variables on stack (memory)
+	 */
+
+	.global asm_strncmp
+
+.text
+asm_strncmp:
+	pushq %rbp		# Routine preamble
+	movq %rsp, %rbp
+	pushq %rbx		# Save registers to be used
+	pushq %rcx
+	xorl %ecx, %ecx		# Start a counter
+
+loop:
+	cmpl %ecx, %edx		# Compare 'n' (3rd arg) with the counter
+	jz equal		# If equal, break
+	movzxb (%rdi, %rcx), %eax	# Char from first argument  (@s1)
+	movzxb (%rsi, %rcx), %ebx	# Char from second argument (@s2)
+	cmpb %bl, %al		# Compare characters
+	jb less_than		# If less than, break
+	ja greater_than		# If greater than, break
+	inc %ecx		# Otherwise, increment the counter
+	# Test if the s1 character is NULL (end of string)
+	test %al, %al		# 'test' (bitwise &) is lighter than 'cmp'
+	jnz loop		# If not NULL (zero) loop, otherwiser break
+
+equal:
+	movl $0, %eax		# If equal return zero
+
+end:
+	popq %rcx		# Restore registers used
+	popq %rbx
+	movq %rbp, %rsp		# Routine epilogue
+	popq %rbp
+	ret
+
+less_than:
+	movl $-1, %eax		# If first less than second return -1
+	jmp end
+
+greater_than:
+	movl $1, %eax		# If first greater return 1
+	jmp end
