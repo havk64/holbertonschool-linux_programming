6b3d6871f3bba891980f52113a7e2aeaa4b6dbff
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-11-12 13:47:41 -0800

    Add strncasecmp version using gas syntax/assembler

---
+++ 0x09-libasm/gas/103-strncasecmp.s
@@ -0,0 +1,67 @@
+
+	/*
+	 * asm_strncasecmp - clone of strncasecmp function in X86 Assembly
+	 *
+	 * Description:	compares the two strings @s1 and @s2, ignoring the case
+	 * of the characters.
+	 *
+	 * Prototype: int asm_strncasecmp(const char *s1, const char *s2, size_t n);
+	 * @s1: a pointer to the first string
+	 * @s2: a pointer to the second string
+	 * @n: the number of bytes to be compared
+	 * Return: It returns an integer less than, equal to, or greater than
+	 * zero if s1 is found, respectively, to be less than, to match, or be
+	 * greater than s2.compare two strings ignoring case
+	 */
+
+	.globl asm_strncasecmp
+
+.text
+asm_strncasecmp:
+	pushq %rbp
+	movq %rsp, %rbp
+	pushq %rbx
+	pushq %rcx
+	xor %ecx, %ecx
+
+loop:
+	cmpl %ecx, %edx
+	jz break
+	movzxb (%edi, %ecx), %eax
+	cmpb $0x5b, %al
+	jb downcase1
+first:	movzxb (%esi, %ecx), %ebx
+	cmpb $0x5b, %bl
+	jb downcase2
+cmp:	cmpb %bl, %al
+	jnz diff
+	inc %ecx
+	test %al, %al
+	jnz loop
+
+end:
+	popq %rcx
+	popq %rbx
+	movq %rbp, %rsp
+	popq %rbp
+	ret
+
+diff:
+	sub %ebx, %eax
+	jmp end
+
+downcase1:
+	cmpb $0x40, %al
+	jna first
+	or $0x20, %al
+	jmp first
+
+downcase2:
+	cmpb $0x40, %bl
+	jna cmp
+	or $0x20, %bl
+	jmp cmp
+
+break:
+	xor %eax, %eax
+	jmp end
