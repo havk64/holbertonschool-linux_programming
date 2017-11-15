f2da580a6a6ebf46b5ff1ab8042ae1bb1912a5b2
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-11-15 06:52:40 -0800

    Use just r8 register in case checking, improve readability

--- 0x09-libasm/103-strncasecmp.asm
+++ 0x09-libasm/103-strncasecmp.asm
@@ -25,23 +25,20 @@ asm_strncasecmp:
 	push rbx		; Preserve register's state
 	push rcx
 	push r8
-	push r9
 	xor ecx, ecx		; Clear the counter (zero)
 
 loop:
 	cmp edx, ecx		; When the counter is equal to @n...
 	jz break		; Stop the loop
 	movzx eax, BYTE [edi + ecx] ; Read @s1 character
-	xor r8d, r8d		    ; Clear edx, start case check within range (A-Z)
-	lea r9d, [eax - 'A']	    ; By subt the lower bound we skip one comparison
-	cmp r9b, 'Z' - 'A'	    ; The only comparison needed  now is with max - min
+	lea r8d, [eax - 'A']	    ; By subt the lower bound we skip one comparison
+	cmp r8b, 'Z' - 'A'	    ; The only comparison needed  now is with max - min
 	setbe r8b		    ; Using setxx we avoid unnecessary jumps
 	shl r8b, 5		    ; Set the 6th bit to be activated case upper case
 	or al, r8b		    ; Use *or* to activate the 6th bit/lower the case
 	movzx ebx, BYTE [esi + ecx] ; Do the same with the second string @s2
-	xor r8d, r8d
-	lea r9d, [ebx - 'A']
-	cmp r9b, 'Z' - 'A'
+	lea r8d, [ebx - 'A']
+	cmp r8b, 'Z' - 'A'
 	lea r8d, [ebx + 0x20]	; This time, instead of setxx I'm using LEA...
 	cmovbe ebx, r8d		; and conditional move to lower the case (when upper)
 cmp:	cmp al, bl		; Check if they diff
@@ -51,8 +48,7 @@ cmp:	cmp al, bl		; Check if they diff
 	jnz loop		; ...loop
 
 end:
-	pop r9			; Restore register's state/values
-	pop r8
+	pop r8			; Restore register's state/values
 	pop rcx
 	pop rbx
 	mov rsp, rbp		; Routine prologue
