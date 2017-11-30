f41637f5a9ef87cd5a929ca208c40c9afb2d5dd5
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-11-29 21:22:50 -0800

    Add loop program which will have a string in the heap to be hacked

---
+++ 0x08-malloc/hackvm/hack_heap/loop.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	char *s;
+	unsigned long int i;
+	pid_t pid;
+
+	pid = getpid();
+	s = strdup("Holberton");
+	if (s == NULL)
+	{
+		fprintf(stderr, "Can't allocate mem with malloc\n");
+		return (EXIT_FAILURE);
+	}
+	i = 0;
+	while (s)
+	{
+		printf("PID: %d, [%3lu] %s (%p)\n", pid, i, s, (void *)s);
+		sleep(1);
+		i++;
+	}
+	return (EXIT_SUCCESS);
+}
