599387fd1f19cf49ca04b8f97de9041b08360fda
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-04 09:06:25 -0800

    Add purpose description to include files on header

--- 0x08-malloc/hackvm/hack_heap/lcap.h
+++ 0x08-malloc/hackvm/hack_heap/lcap.h
@@ -1,8 +1,11 @@
 #ifndef LCAP_H
 #define LCAP_H
-#include <sys/capability.h>
-#include <sys/types.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <sys/capability.h>	/* for capability functions */
+#include <sys/types.h>		/* for extra types */
+#include <stdio.h>		/* for printf, sprintf */
+#include <fcntl.h>		/* for open */
+#include <unistd.h>		/* for close */
+#include <sys/ptrace.h>		/* for ptrace */
+#include <stdlib.h>		/* for atoi */
+#include <sys/wait.h>		/* for waitpid */
 #endif /* LCAP_H */
