5f99327560d948f1b1c16469f3723610b226bb22
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-05 17:19:47 -0800

    Adjust the loop output

--- 0x08-malloc/hackvm/hack_heap/loop.c
+++ 0x08-malloc/hackvm/hack_heap/loop.c
@@ -19,7 +19,7 @@ int main(void)
 	i = 0;
 	while (s)
 	{
-		printf("PID: %d, [%3lu] %s (%p)\n", pid, i, s, (void *)s);
+		printf("PID: %d [%lu] %s (%p)\n", pid, i, s, (void *)s);
 		sleep(1);
 		i++;
 	}
