ccaf2094f2904445feed40ff0689fd91652d4fcd
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-05 17:15:12 -0800

    Replace setuid with cap_sys_ptrace capability

--- 0x08-malloc/hackvm/hack_heap/lcap.h
+++ 0x08-malloc/hackvm/hack_heap/lcap.h
@@ -1,11 +1,8 @@
 #ifndef LCAP_H
 #define LCAP_H
-#include <sys/capability.h>	/* for capability functions */
 #include <sys/types.h>		/* for extra types */
 #include <stdio.h>		/* for printf, sprintf */
 #include <fcntl.h>		/* for open */
 #include <unistd.h>		/* for close */
-#include <sys/ptrace.h>		/* for ptrace */
 #include <stdlib.h>		/* for atoi */
-#include <sys/wait.h>		/* for waitpid */
 #endif /* LCAP_H */
--- 0x08-malloc/hackvm/hack_heap/write_heap.c
+++ 0x08-malloc/hackvm/hack_heap/write_heap.c
@@ -1,86 +1,28 @@
-#include <stdio.h>	/* for sprintf */
-#include <fcntl.h>	/* for open */
-#include <unistd.h>	/* for close */
-#include <sys/ptrace.h>	/* for ptrace */
-#include <stdlib.h>	/* for atoi */
-#include <sys/wait.h>	/* for waitpid */
-
-static uid_t uid, euid;
-
-void do_setuid(void)
-{
-	int st;
-	uid_t _uid, _euid;
-
-#ifdef _POSIX_SAVED_IDS
-	st = seteuid(euid);
-#else
-	st = setreuid(uid, euid);
-#endif
-	if (st < 0)
-		perror("setuid error (do)");
-
-	_uid = getuid();
-	_euid = geteuid();
-	printf("RUID: %u, EUID: %u\n", _uid, _euid);
-}
-
-void undo_setuid(void)
-{
-	int st;
-	uid_t _uid, _euid;
-
-#ifdef _POSIX_SAVED_IDS
-	st = seteuid(uid);
-#else
-	st = setreuid(euid, uid);
-#endif
-	if (st < 0)
-		perror("setuid error (undo)");
-
-	_uid = getuid();
-	_euid = geteuid();
-	printf("RUID: %u, EUID: %u\n", _uid, _euid);
-}
+#include "lcap.h"
 
 int main(int ac, char *argv[])
 {
-	int fd, mem;
-	pid_t pid;
+	int fd, mem, n;
 	char s[64];
-	long pt;
 
-	uid = getuid();
-	euid = geteuid();
-	printf("RUID: %u, EUID: %u\n", uid, euid);
-	undo_setuid();
 	if (ac < 3)
 	{
-		printf("Usage: ./%s <pid> <offset>\n", __FILE__);
+		printf("Usage: ./%s <pid> <offset>\n", argv[0]);
 		return (EXIT_FAILURE);
 	}
 
-	pid = atoi(argv[1]);
 	sprintf(s, "/proc/%s/mem", argv[1]);
 
-	do_setuid();
 	fd = open(s, O_RDWR);
 	if (fd < 0)
 		perror("Error opening file");
 
-	pt = ptrace(PTRACE_ATTACH, pid, NULL, NULL);
-	undo_setuid();
-	if (pt < 0)
-		perror("Ptrace error");
-
-	waitpid(pid, NULL, 0);
-
 	mem = atoi(argv[2]);
-	printf("Mem: %x\n", mem);
 	lseek(fd, mem, SEEK_SET);
-	write(fd, "holberton", 10);
+	n = write(fd, "Holberton", 10);
+	if (n == 10)
+		printf("Hacked!\n");
 
-	ptrace(PTRACE_DETACH, pid, NULL, NULL);
 	close(fd);
 	return (EXIT_SUCCESS);
 }
