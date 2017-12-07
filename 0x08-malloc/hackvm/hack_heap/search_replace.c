22bafbc4aff6d68b26865c3e4189461f96cb2e58
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-06 18:04:06 -0800

    Add more items to output, variable names

--- 0x08-malloc/hackvm/hack_heap/search_replace.c
+++ 0x08-malloc/hackvm/hack_heap/search_replace.c
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 	pid_t pid;
-	char maps_file[msize], *line;
+	char maps_path[64], maps_line[lsize], mem_path[64], *line;
 	FILE *maps;
 
 	if (argc < 4)
@@ -14,15 +14,18 @@ int main(int argc, char *argv[])
 	}
 
 	pid = atoi(argv[1]);
-	sprintf(maps_file, "/proc/%d/maps", pid);
-	maps = fopen(maps_file, "r");
+	sprintf(maps_path, "/proc/%d/maps", pid);
+	sprintf(mem_path, "/proc/%d/mem", pid);
+	printf("[*] maps: %s\n", maps_path);
+	printf("[*] mem: %s\n", mem_path);
+	maps = fopen(maps_path, "r");
 	if (maps == NULL)
 	{
 		perror("fopen");
 		return (EXIT_FAILURE);
 	}
 
-	while ((line = fgets(maps_file, msize, maps)))
+	while ((line = fgets(maps_line, lsize, maps)))
 	{
 		if (strstr(line, "[heap]") != NULL)
 			printf("%s", line);
