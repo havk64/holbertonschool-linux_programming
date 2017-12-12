395b1152b98d3d7b13d2fdb3460396e86427713e
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-11 19:45:51 -0800

    Add naive.h file, first commit

---
+++ 0x08-malloc/EYNTK/naive.h
@@ -0,0 +1,10 @@
+#ifndef NAIVE_H
+#define NAIVE_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#define BRK_FAILED ((void *)-1)
+#define bsize 128
+int print_mem_map(void);
+#endif /*NAIVE_H */
