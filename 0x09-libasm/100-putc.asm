9ef09eb84cca786b4a1d8911769c55e7fc9ad9f1
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-15 17:40:16 -0800

    Improve comments

--- 0x09-libasm/100-putc.asm
+++ 0x09-libasm/100-putc.asm
@@ -27,8 +27,8 @@ asm_putc:
 	push rcx		; Save registers state/values
 	push rdx
 	push rsi
-	push rdi		; Copy the 1st arg to putc to the stack
-	mov eax, 1		; eax carry the syscall number (1 for write)
+	push rdi		; Save the 1st arg to putc onto the stack
+	mov eax, 1		; %eax register carries the syscall number (1 for write)
 	mov edx, 1		; 3rd arg is the amount of bytes to be written
 	lea rsi, [rsp]		; 2nd arg is a pointer to the buffer to be printed
 	mov edi, 1		; 1st arg is the number of file descriptor (1 for STDOUT)
