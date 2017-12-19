7a805558e9a4564f51aacfb0d2f02ebdee48f9cb
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-18 17:49:42 -0800

    Update header file, add function prototype

--- 0x08-malloc/EYNTK/naive.h
+++ 0x08-malloc/EYNTK/naive.h
@@ -15,4 +15,5 @@ int get_key(int no_timeout);
 int prompt_mem_map(pid_t pid);
 void *naive_malloc(size_t size);
 void *naive_malloc_header(size_t size);
+void *naive_malloc_page(size_t size);
 #endif /*NAIVE_H */
