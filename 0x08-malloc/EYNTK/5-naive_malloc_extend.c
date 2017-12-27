d2ce6c660a3dfb5c1ff8e214a6cc02028ae6bc77
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-26 20:01:03 -0800

    Complementing docstring description

--- 0x08-malloc/EYNTK/5-naive_malloc_extend.c
+++ 0x08-malloc/EYNTK/5-naive_malloc_extend.c
@@ -1,7 +1,7 @@
 #include "naive.h"
 
 /**
- * naive_malloc_extend - a naive malloc-like function
+ * naive_malloc_extend - a naive malloc-like function, version 5
  * @size: the size to be allocated
  *
  * Return: On success, a void pointer to beginning of allocated space on memory
