1e9e323119d364fe4ff5f1da5aecf06f5748d5b5
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2017-12-27 12:09:09 -0800

    Add more tests/assertions to task 4 (edge cases)

--- 0x09-libasm/tests/4-main.c
+++ 0x09-libasm/tests/4-main.c
@@ -5,9 +5,11 @@
 
 #include "libasm.h"
 
-#define S1  "Holberton School"
+#define S1  "Holberton School Socool"
 #define S2  "School"
 #define S3  "Socool"
+#define S4  "Socooll"
+#define S5  ""
 
 /**
  * main - Program entry point
@@ -16,6 +18,8 @@
  */
 int main(void)
 {
+	assert(strstr(S1, S5) == asm_strstr(S1, S5));
+	assert(strstr(S1, S4) == asm_strstr(S1, S4));
 	assert(strstr(S1, S2) == asm_strstr(S1, S2));
 	assert(strstr(S1, S3) == asm_strstr(S1, S3));
 	assert(strstr(S1, S1) == asm_strstr(S1, S1));
