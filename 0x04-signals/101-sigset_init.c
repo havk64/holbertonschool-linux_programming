f408aef646fe8c621375333635c4ad914cf12022
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-04-07 12:16:56 -0700

    Task 13 completed, first try

--- 0x04-signals/101-sigset_init.c
+++ 0x04-signals/101-sigset_init.c
@@ -2,5 +2,13 @@
 
 int sigset_init(sigset_t *set, int *signals)
 {
+	int i;
 
+	sigemptyset(set);
+	for (i = 0; signals[i] != 0; i++)
+	{
+		if (sigaddset(set, signals[i]) < 0)
+			return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
