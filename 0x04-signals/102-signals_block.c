a702352517446c2c594d9f3e4c7dbdfa2c715c51
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-04-07 16:23:35 -0700

    Add docstring to functions

--- 0x04-signals/102-signals_block.c
+++ 0x04-signals/102-signals_block.c
@@ -1,5 +1,11 @@
 #include "signals.h"
 
+/**
+ * signals_block - blocks a given set of signals from being delivered to the
+ * current process
+ * @signals: a pointer to a zero terminated array of integers (signals)
+ * Return: On success, 0. On failure, -1
+ */
 int signals_block(int *signals)
 {
 	int i, how;
