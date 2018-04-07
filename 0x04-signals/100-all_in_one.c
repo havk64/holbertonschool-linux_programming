c9c54e9f37fcc0c063c5675d90ae5c481573a095
Alexandro de Oliveira <alexandro.oliveira@holbertonschool.com>
2018-04-06 18:02:25 -0700

    Task 12 completed

--- 0x04-signals/100-all_in_one.c
+++ 0x04-signals/100-all_in_one.c
@@ -1,6 +1,30 @@
 #include "signals.h"
 
+void sigaction_handler(int signum, siginfo_t *info, void *context)
+{
+	char *msg = "Caught";
+
+	UNUSED(signum);
+	UNUSED(context);
+	psiginfo(info, msg);
+	fflush(stdout);
+}
+
 void all_in_one(void)
 {
+	int i;
+	struct sigaction action;
+
+	for (i = 1; i < SIGNUM; i++)
+	{
+		if (i != 9 && i != 19)
+		{
+			action.sa_sigaction = sigaction_handler;
+			sigemptyset(&action.sa_mask);
+			action.sa_flags = SA_SIGINFO;
+			if (sigaction(i, &action, NULL) < 0)
+				continue;
+		}
+	}
 
 }
